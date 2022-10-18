4e4830996f8ec064f5cd700b8862a4e91a4a0e4d
AyseSinegu <114016463+AyseSinegu@users.noreply.github.com>
2022-10-18 22:41:55 +0300

    Add files via upload

---
+++ interfaceDemo/src/interfaceDemo/Worker.java
@@ -0,0 +1,15 @@
+package interfaceDemo;
+
+public class Worker implements IWorkable, IEatable, IPayable{
+	public void work() {
+		
+	}
+	public void eat() {
+		
+	}
+	public void pay() {
+		
+	}
+	
+
+}
