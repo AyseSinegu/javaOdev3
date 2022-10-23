56660877106c98065ed959f298c84222e4a2a911
AyseSinegu <114016463+AyseSinegu@users.noreply.github.com>
2022-10-23 23:53:51 +0300

    Add files via upload

---
+++ GameProject/IUserValidation.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject
+{
+    interface IUserValidation
+    {
+        void Validate(Gamer gamer);
+    }
+}
